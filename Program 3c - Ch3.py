d92eb1309c4f410ddbb52c2775168490e535ab1c
Raven Chromatic <111197001+ravenchromatic@users.noreply.github.com>
2022-09-16 22:49:11 -0500

    Update Program 3c - Ch3.py

--- Program 3c - Ch3.py
+++ Program 3c - Ch3.py
@@ -1,12 +1,12 @@
 while True:
 #  gotta get that INPUT!!!
-year = float(input('Enter a year: '))
-if year%100 == 0 and year%400 == 0:
-    print('In %d February has 29 days' %year)
-elif year%100 != 0 and year%4 == 0:
-    print('In %d February has 29 days'%year)
-else:
-    print('In %d February has 28 days'%year)
+    year = float(input('Enter a year: '))
+    if year%100 == 0 and year%400 == 0:
+        print('In %d February has 29 days' %year)
+    elif year%100 != 0 and year%4 == 0:
+        print('In %d February has 29 days'%year)
+    else:
+        print('In %d February has 28 days'%year)
 
 
         
